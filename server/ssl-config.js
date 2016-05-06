0031d71327c25c003ec6e9839992f78df0f178b1
Ryan Graham <r.m.graham@gmail.com>
2016-05-05 19:08:54 -0700

    update copyright notices and license

--- server/ssl-config.js
+++ server/ssl-config.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-example-ssl
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 var path = require('path');
 var fs = require('fs');
 
--- server/test-server.js
+++ server/test-server.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-example-ssl
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 /**
  * Run `node import.js` to import the test data into the db.
  */
